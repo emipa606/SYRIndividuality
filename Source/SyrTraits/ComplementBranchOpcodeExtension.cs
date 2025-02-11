577ddd8078b0ab2b4e92c19f7386a2a74e61078a
Choosechee <108241707+Choosechee@users.noreply.github.com>
2025-02-10 22:05:55 -0600

    Fixed Slow Learner and Creative Thinker
    Slow learner and creative thinker now apply their offsets before trait factors, improving consistency with other traits and fixing a major bug with slow learner.
    Slow learner code was reworked to not require a -130% offset.
    The stat explanations for slow learner and creative thinker now look different.
    Removed some language strings that are now unnecessary.
    Currently, the code fixes only affect the 1.5 version of the mod. The XML changes have been copied over to the other versions, but I need help or guidance on porting the code changes.

---
+++ Source/SyrTraits/ComplementBranchOpcodeExtension.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using Verse;
+
+namespace SyrTraits
+{
+    internal static class ComplementBranchOpcodeExtension
+    {
+        private readonly struct ComplementOpCodeInfo(string name, bool standalone = false, string requiredNewPart = null, string requiredRemovePart = null)
+        {
+            public readonly string name = name;
+            public readonly bool standalone = standalone && requiredNewPart is null && requiredRemovePart is null;
+            public readonly string requiredNewPart = requiredNewPart;
+            public readonly string requiredRemovePart = requiredRemovePart;
+        }
+
+        private static readonly Dictionary<string, ComplementOpCodeInfo> _complementOpCodes = new()
+        {
+            ["Br"] = new("Nop", standalone: true),
+            ["Brfalse"] = new("Brtrue"),
+            ["Brtrue"] = new("Brfalse"),
+            ["Beq"] = new("Bne", requiredNewPart: "Un"),
+            ["Bne"] = new("Beq", requiredRemovePart: "Un"),
+            ["Blt"] = new("Bge"),
+            ["Bge"] = new("Blt"),
+            ["Bgt"] = new("Ble"),
+            ["Ble"] = new("Bgt"),
+        };
+        
+        public static OpCode Complement(this OpCode opCode)
+        {
+            string[] splitOpCodeName = CaptalizeFirstSplit(opCode.Name, '.');
+            string mainPart = splitOpCodeName[0];
+            ComplementOpCodeInfo complement = _complementOpCodes.GetValueOrDefault(mainPart);
+
+            if (complement.standalone)
+                splitOpCodeName = [complement.name];
+            else
+                splitOpCodeName[0] = complement.name;
+
+            if (complement.requiredNewPart is not null && !splitOpCodeName.Contains(complement.requiredNewPart))
+            {
+                string[] newSplitOpCodeName = new string[splitOpCodeName.Length + 1];
+                newSplitOpCodeName[0] = splitOpCodeName[0];
+                newSplitOpCodeName[1] = complement.requiredNewPart;
+
+                for (int i = 1; i < splitOpCodeName.Length; i++)
+                    newSplitOpCodeName[i + 1] = splitOpCodeName[i];
+
+                splitOpCodeName = newSplitOpCodeName;
+            }
+            if (complement.requiredRemovePart is not null && splitOpCodeName.Contains(complement.requiredRemovePart))
+            {
+                string[] newSplitOpCodeName = new string[splitOpCodeName.Length - 1];
+
+                int index;
+                for (index = 0; splitOpCodeName[index] != complement.requiredRemovePart; index++)
+                    newSplitOpCodeName[index] = splitOpCodeName[index];
+
+                int indexOfBadPart = index;
+                for (index = indexOfBadPart + 1; index < splitOpCodeName.Length; index++)
+                    newSplitOpCodeName[index - 1] = splitOpCodeName[index];
+
+                splitOpCodeName = newSplitOpCodeName;
+            }
+
+            string complementOpCodeName = string.Join("_", splitOpCodeName);
+            FieldInfo complementOpCodeField = typeof(OpCodes).GetField(complementOpCodeName);
+
+            if (complementOpCodeField is null)
+                throw new ArgumentException($"Could not find complement of {opCode.Name}.", nameof(opCode));
+            return (OpCode)complementOpCodeField.GetValue(null);
+        }
+
+        private static string[] CaptalizeFirstSplit(string str, char separator)
+        {
+            string[] split = str.Split(separator);
+            for (int i = 0; i < split.Length; i++)
+                split[i] = split[i].CapitalizeFirst();
+
+            return split;
+        }
+    }
+}
--- Source/SyrTraits/HarmonyPatches/StatWorker_GetExplanationUnfinalized.cs
+++ Source/SyrTraits/HarmonyPatches/StatWorker_GetExplanationUnfinalized.cs
@@ -1,61 +1,159 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
 using System.Text;
 using HarmonyLib;
 using RimWorld;
-using UnityEngine;
 using Verse;
 
 namespace SyrTraits;
 
+using static StatWorker_GetValueUnfinalized.StatBonuses;
+
 [HarmonyPatch(typeof(StatWorker), nameof(StatWorker.GetExplanationUnfinalized))]
 public static class StatWorker_GetExplanationUnfinalized
 {
-    public static void Postfix(ref string __result, StatWorker __instance, StatRequest req,
-        StatDef ___stat)
+    private static readonly FieldInfo _storyField = typeof(Pawn).GetField(nameof(Pawn.story));
+    private static readonly FieldInfo _traitsField = typeof(Pawn_StoryTracker).GetField(nameof(Pawn_StoryTracker.traits));
+    private static readonly FieldInfo _creativeThinkerField = typeof(SyrTraitDefOf).GetField(nameof(SyrTraitDefOf.SYR_CreativeThinker));
+    private static readonly MethodInfo _hasTraitMethod = typeof(TraitSet).GetMethod(nameof(TraitSet.HasTrait), [typeof(TraitDef)]);
+    private static readonly MethodInfo _countGetter = typeof(List<Trait>).PropertyGetter(nameof(List<Trait>.Count));
+    private static readonly FieldInfo _statField = typeof(StatWorker).Field("stat");
+    private static readonly MethodInfo _addCustomTraitsExplanationsMethod = typeof(StatWorker_GetExplanationUnfinalized).Method(nameof(AddCustomTraitsExplanations));
+
+    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
     {
-        var pawn = req.Thing as Pawn;
-        if (pawn?.story?.traits == null || ___stat == null)
+        CodeMatcher codeMatcher = new CodeMatcher(instructions);
+
+        // Finding the required variables
+        Func<CodeInstruction, bool> stringBuilderPredicate = instruction =>
         {
+            if (instruction.opcode != OpCodes.Newobj)
+                return false;
+
+            ConstructorInfo constructor = (ConstructorInfo)instruction.operand;
+            return typeof(StringBuilder).IsAssignableFrom(constructor.DeclaringType);
+        };
+        CodeMatch stringBuilderMatch = new CodeMatch(stringBuilderPredicate);
+        codeMatcher.MatchStartForward(stringBuilderMatch);
+        codeMatcher.MatchStartForward(CodeMatch.StoresLocal());
+        codeMatcher.ThrowIfInvalid($"{SyrIndividuality.logPrefix} Could not find StringBuilder variable in StatWorker.GetExplanationUnfinalized");
+        int stringBuilderLocalIndex = codeMatcher.Instruction.LocalIndex();
+        
+        CodeMatch pawnMatch = new CodeMatch(OpCodes.Isinst, typeof(Pawn));
+        codeMatcher.MatchStartForward(pawnMatch);
+        codeMatcher.MatchStartForward(CodeMatch.StoresLocal());
+        codeMatcher.ThrowIfInvalid($"{SyrIndividuality.logPrefix} Could not find Pawn variable in StatWorker.GetExplanationUnfinalized");
+        int pawnLocalIndex = codeMatcher.Instruction.LocalIndex();
+
+        // Entering the relevant section
+        codeMatcher.MatchStartForward(CodeMatch.LoadsField(_traitsField));
+        codeMatcher.ThrowIfInvalid($"{SyrIndividuality.logPrefix} Could not find load field instruction for Pawn_StoryTracker.traits in StatWorker.GetExplanationUnfinalized");
+
+        codeMatcher.MatchStartForward(CodeMatch.LoadsConstant("StatsReport_RelevantTraits"));
+        codeMatcher.MatchStartBackwards(new CodeMatch(instruction => instruction.labels?.Count > 0));
+        codeMatcher.ThrowIfInvalid($"{SyrIndividuality.logPrefix} Could not find jump location for relevant traits explanations in StatWorker.GetExplanationUnfinalized");
+        List<Label> labels = codeMatcher.Instruction.labels;
+
+        /* 
+         * We need to change the condition for the trait offsets and factors code
+         * to be run, because Creative Thinker would not make the code run since
+         * the offsets it gives are all custom code and don't use the normal system
+         * at all
+        */
+        codeMatcher.MatchStartBackwards(new CodeMatch(instruction =>
+            instruction.operand is Label label && labels.Contains(label)));
+        codeMatcher.ThrowIfInvalid($"{SyrIndividuality.logPrefix} Could not find branch to relevant traits explanations in StatWorker.GetExplanationUnfinalized");
+        Label relevantTraitsLabel = (Label)codeMatcher.Instruction.operand;
+
+        codeMatcher.MatchStartForward(new CodeMatch(instruction =>
+            instruction.operand is Label label && !labels.Contains(label)));
+        codeMatcher.ThrowIfInvalid($"{SyrIndividuality.logPrefix} Could not find branch that avoids relevant traits explanations in StatWorker.GetExplanationUnfinalized");
+        Label noRelevantTraitsLabel = (Label)codeMatcher.Instruction.operand;
+        codeMatcher.Opcode = codeMatcher.Opcode.Complement();
+        codeMatcher.Operand = relevantTraitsLabel;
+
+        codeMatcher.Advance(1);
+        List<CodeInstruction> newConditionInstructions =
+        [
+            new CodeInstruction(OpCodes.Ldloc_S, pawnLocalIndex),
+            new CodeInstruction(OpCodes.Ldfld, _storyField),
+            new CodeInstruction(OpCodes.Ldfld, _traitsField),
+            new CodeInstruction(OpCodes.Ldsfld, _creativeThinkerField),
+            new CodeInstruction(OpCodes.Callvirt, _hasTraitMethod),
+            new CodeInstruction(OpCodes.Brfalse, noRelevantTraitsLabel)
+        ];
+        codeMatcher.InsertAndAdvance(newConditionInstructions);
+
+        // Adding the new explanations
+        codeMatcher.MatchStartForward(new CodeMatch() { opcodeSet = [OpCodes.Br, OpCodes.Br_S] });
+        codeMatcher.ThrowIfInvalid($"{SyrIndividuality.logPrefix} Could not find start branch to trait offsets loop.");
+        Label traitOffsetsConditionLabel = (Label)codeMatcher.Instruction.operand;
+
+        CodeMatch tocLabelMatch = new CodeMatch(ci => ci.labels.Contains(traitOffsetsConditionLabel));
+        codeMatcher.MatchStartForward(tocLabelMatch);
+        codeMatcher.ThrowIfInvalid($"{SyrIndividuality.logPrefix} Could not find start of trait offsets loop condition.");
+
+        codeMatcher.MatchStartForward(CodeMatch.Branches());
+        codeMatcher.ThrowIfInvalid($"{SyrIndividuality.logPrefix} Could not branch to trait offsets loop body.");
+        codeMatcher.Advance(1); // we are right after the trait offsets loop now
+
+        List<CodeInstruction> newInstructions = new List<CodeInstruction>()
+        {
+            new CodeInstruction(OpCodes.Ldarg_0),
+            new CodeInstruction(OpCodes.Dup),
+            new CodeInstruction(OpCodes.Ldfld, _statField),
+            new CodeInstruction(OpCodes.Ldarg_1),
+            new CodeInstruction(OpCodes.Ldloc_S, stringBuilderLocalIndex),
+            new CodeInstruction(OpCodes.Call, _addCustomTraitsExplanationsMethod)
+        };
+        codeMatcher.Insert(newInstructions);
+
+        return codeMatcher.InstructionEnumeration();
+    }
+
+    private static void AddCustomTraitsExplanations(StatWorker instance, StatDef stat, StatRequest req, StringBuilder stringBuilder)
+    {
+        Pawn pawn = req.Thing as Pawn;
+        if (pawn.skills?.skills is null || pawn.def.statBases is null)
             return;
-        }
 
-        float num;
-        float val;
-        StringBuilder stringBuilder;
-        if (___stat == StatDefOf.ResearchSpeed && pawn.story.traits.HasTrait(SyrTraitDefOf.SYR_CreativeThinker) &&
-            pawn.skills != null && pawn.def.statBases != null)
+        TraitSet pawnTraits = pawn.story.traits;
+        if (stat == StatDefOf.GlobalLearningFactor && pawnTraits.HasTrait(SyrTraitDefOf.SlowLearner))
         {
-            num = 1f;
-            if (pawn.def.statBases.Find(x => x?.stat != null && x.stat == ___stat) != null)
-            {
-                num = pawn.def.statBases.Find(x => x?.stat != null && x.stat == ___stat).value;
-            }
-
-            val = 0.115f * pawn.skills.GetSkill(SkillDefOf.Artistic).Level * num;
-            stringBuilder = new StringBuilder();
-            stringBuilder.AppendLine("SyrTraitsDaVinciGene".Translate());
-            stringBuilder.AppendLine(
-                $"{"    " + SkillDefOf.Artistic.LabelCap + " ("}{pawn.skills.GetSkill(SkillDefOf.Artistic).Level}): {val.ToStringSign()}{__instance.ValueToString(val, false)}");
-            __result += stringBuilder.ToString();
-            return;
+            Trait slowLearner = pawnTraits.GetTrait(SyrTraitDefOf.SlowLearner);
+            int totalSkillLevel = TotalSkillLevel(pawn);
+            float slowLearnerTotalOffset = slowLearner.OffsetOfStat(stat)
+                                         + SlowLearnerBonus(totalSkillLevel);
+            int insertionIndex = RemoveSlowLearnerStatDisplay(stringBuilder, slowLearner);
+            
+            stringBuilder.Insert(insertionIndex, $" ({"SyrTraitsSlowLearner".Translate()}: {totalSkillLevel}): {slowLearnerTotalOffset.ToStringSign()}{instance.ValueToString(slowLearnerTotalOffset, false)}");
         }
-
-        if (___stat != StatDefOf.GlobalLearningFactor || !pawn.story.traits.HasTrait(SyrTraitDefOf.SlowLearner) ||
-            pawn.skills == null || pawn.def.statBases == null)
+        else if (stat == StatDefOf.ResearchSpeed && pawnTraits.HasTrait(SyrTraitDefOf.SYR_CreativeThinker))
         {
-            return;
-        }
+            Trait creativeThinker = pawnTraits.GetTrait(SyrTraitDefOf.SYR_CreativeThinker);
+            float creativeThinkerBonus = CreativeThinkerBonus(pawn);
 
-        num = 0f;
-        foreach (var skill in pawn.skills.skills)
-        {
-            num += skill.levelInt;
+            stringBuilder.Append("    " + creativeThinker.LabelCap);
+            stringBuilder.AppendLine($" ({SkillDefOf.Artistic.LabelCap}: {pawn.skills.GetSkill(SkillDefOf.Artistic).Level}): {creativeThinkerBonus.ToStringSign()}{instance.ValueToString(creativeThinkerBonus, false)}");
         }
+    }
 
-        val = 0.02f * Mathf.Clamp(num, 40f, 140f);
-        stringBuilder = new StringBuilder();
-        stringBuilder.AppendLine("SyrTraitsSlowLearnerLabel".Translate());
-        stringBuilder.AppendLine(
-            $"{"    " + "SyrTraitsSlowLearner".Translate() + " ("}{num}): {val.ToStringSign()}{__instance.ValueToString(val, false)}");
-        __result += stringBuilder.ToString();
+    // removes the colon and everything after it (on the same line), and returns where the colon was
+    private static int RemoveSlowLearnerStatDisplay(StringBuilder stringBuilder, Trait slowLearnerTrait)
+    {
+        string stringBuilderString = stringBuilder.ToString();
+        string slowLearnerLabel = slowLearnerTrait.LabelCap;
+        int slowLearnerIndex = stringBuilderString.IndexOf(slowLearnerLabel);
+        
+        if (slowLearnerIndex < 0)
+            throw new ArgumentException($"Could not find {slowLearnerLabel} in {nameof(stringBuilder)}");
+
+        int statDisplayStart = slowLearnerIndex + slowLearnerLabel.Length;
+        int statDisplayEnd = stringBuilderString.IndexOf(Environment.NewLine, statDisplayStart);
+        stringBuilder.Remove(statDisplayStart, statDisplayEnd - statDisplayStart);
+
+        return statDisplayStart;
     }
 }
